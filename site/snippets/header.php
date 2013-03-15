44f93d85c59761cb4dd2f0cb57ce04cd4dcf4d59
er1chu <ericjhu@gmail.com>
2013-03-15 01:43:48 -0400

    added some files

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,13 +1,19 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<html lang="en" class="no-js">
 <head>
   
   <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
-
+  <meta name="viewport" content="width=device-width">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
   <?php echo css('http://yui.yahooapis.com/3.8.0/build/cssreset/cssreset-min.css') ?>
   <?php echo css('assets/styles/min/global.css') ?>
   <?php echo css('assets/styles/min/carousel.min.css') ?>
@@ -17,6 +23,9 @@
 </head>
 
 <body>
+  <!--[if lt IE 7]>
+      <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+  <![endif]-->
 
   <div class="row">
   <header>
