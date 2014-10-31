e56929fe6dd75c43c4ec75df81fdeff15a515c4a
Eric Hu <ericjhu@gmail.com>
2014-10-31 19:38:14 -0400

    lazy load fade in, footer change, input change

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,15 +1,15 @@
 <footer>
-<div class="column-3"><a href="http://twitter.com/_erichu" target="_blank">Twitter</a> / <a href="http://facebook.com/seiyohu" target="_blank">Facebook</a> / <a href="http://e-r-h.tumblr.com" target="_blank">Blog</a> /
-			<a href="https://github.com/er1chu" target="_blank">Github</a><br>Design and Development: Eric Hu<br />Typeface: Bau and Big Caslon<br />Content Management: <a href="http://getkirby.com/" target="_blank">Kirby</a><br /><br />
-	<!-- Begin MailChimp Signup Form -->
-	
-		<div id="mc_embed_signup" class="column-3">
-		For Updates: <br />
+<span class="description colophon-footer">
+	Elsewhere: <a href="http://twitter.com/_erichu" target="_blank">Twitter</a> <a href="http://facebook.com/seiyohu" target="_blank">Facebook</a> <a href="http://instagram.com/_erichu" target="_blank">Instagram</a> <a href="http://github.com/er1chu" target="_blank">Github</a> <a href="http://e-r-h.tumblr.com">Blog</a>. This website is powered by Kirby and set in Bau and Big Caslon. For updates please subscribe.
+</span><span id="mc_embed_signup">
 		<form action="http://erichu.us6.list-manage.com/subscribe/post?u=37e160c2a6118e7447e9e766e&amp;id=8f3de8e83f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required>
 			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
 		</form>
-		</div>
+		</span>
+<!-- <div class="column-3"><a href="http://twitter.com/_erichu" target="_blank">Twitter</a> / <a href="http://facebook.com/seiyohu" target="_blank">Facebook</a> / <a href="http://e-r-h.tumblr.com" target="_blank">Blog</a> /
+			<a href="https://github.com/er1chu" target="_blank">Github</a><br>Design and Development: Eric Hu<br />Typeface: Bau and Big Caslon<br />Content Management: <a href="http://getkirby.com/" target="_blank">Kirby</a><br /><br /> -->
+	
 </footer>
 
 </div>
