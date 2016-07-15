3014313ea097278164bb28095efd5a126cceef9a
er1chu <mail@erichu.info>
2016-07-15 11:52:49 -0400

    switched from kirby to jekyll, installed bourbon, reset

--- site/snippets/about-me.php
+++
@@ -1,4 +0,0 @@
-<section id="about-me">
- <h1><?php echo html($data->title()) ?></h1>
- <?php echo kirbytext($data->text()) ?>
-</section>
--- site/snippets/about.php
+++
@@ -1,6 +0,0 @@
-<section id="about-me" class="row">
-<!--	 <div class="col_8">
-	 	<h1><?php echo html($data->title()) ?></h1>
-		 <?php echo kirbytext($data->text()) ?>
-	</div> -->
-</section>
